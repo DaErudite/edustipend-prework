15048f4538a35393c7bc55d6dba1b9f67ee44035
israel <daerudite01@gmail.com>
2023-05-19 13:17:35 +0200

    another commit

--- server.js
+++ server.js
@@ -6,30 +6,27 @@ const app= express();
 
 app.get('/api/comments',async(req,res)=>{
 
-    if (req.query.limit) {
-
     const response= await fetch('https://jsonplaceholder.typicode.com/comments')
     var comments= await response.json();
     //console.log(comments)
-    
 
+    if (req.query.limit) {
     
         const Limitedcomments = comments.slice(0, req.query.limit);
         res.send(Limitedcomments);
-    }
+    } 
+    else res.send(comments);
 })
 
 app.get('/api/posts',async(req,res)=>{
+    const response= await fetch('https://jsonplaceholder.typicode.com/posts')
+    var posts= await response.json();
 
     if (req.query.limit) {
-
-    const response= await fetch('https://jsonplaceholder.typicode.com/posts')
-    var posts= await response.json(); 
-
     
         const Limitedposts = posts.slice(0, req.query.limit);
         res.send(Limitedposts);
-    }
+    } else res.send(posts)
 })
 
 
